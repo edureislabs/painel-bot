f099ab1843241fb7911e57e412ee08a1078d8fff
Eduardo <204800370+edureislabs@users.noreply.github.com>
2026-04-22 21:55:23 -0300

    atualizando navbar

--- app/page.tsx
+++ app/page.tsx
@@ -78,7 +78,7 @@ export default function Home() {
                         transition={{ type: "spring", stiffness: 400 }}
                     >
                         <Image
-    src="../public/edunex site.png"
+    src="../public/images/edunex site.png"
     alt="EduNex Logo"
     width={140}
     height={40}
