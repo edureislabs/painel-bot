88df418777fcd12d931d135e8ba072c15b935152
Eduardo <204800370+edureislabs@users.noreply.github.com>
2026-04-26 23:10:42 -0300

    atualizando navbar

--- app/api/auth/callback/discord/route.ts
+++
@@ -1,5 +0,0 @@
-// app/api/auth/callback/discord/route.ts
-import { handlers } from "@/auth"
-
-// Exporta apenas o GET e POST do handler (o Discord usa GET no callback)
-export const { GET, POST } = handlers
